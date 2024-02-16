477fb4c73d31ec45c3b5095792a2589a49876765
alejandro <widomlanski@hotmail.com>
2024-02-16 15:40:33 -0300

    06

---
+++ tipos/06-numeros.py
@@ -0,0 +1,16 @@
+numero = 14
+decimal = 1.4
+# j= sería como lo que es i (número imaginario raiz cuadrada de -1 no puede ser)
+imaginario = 2+2j
+
+
+numero += 2  # es lo mismo que nunero = numero +2, se pude hacer con todas las operaciones de debajo
+
+print("numero", numero)
+print(numero+3)
+print(numero-7)
+print(numero*4)
+print(numero/3)
+# al poner dos dividir me va a devolver el primer número sin redondearlo
+print(numero//3)
+print(numero**3)  # hace ** la potencia, en este caso 14 al cubo
